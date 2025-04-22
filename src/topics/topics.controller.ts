41cb6a053ae30b673d4db099215587342af84700
Nikita23t <n.tsarev2014@yandex.ru>
2025-04-22 19:50:30 +0300

    что-то подправил

--- src/topics/topics.controller.ts
+++ src/topics/topics.controller.ts
@@ -1,26 +1,54 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Body, Post, Logger } from '@nestjs/common';
 import { ParserService } from './topics.service';
-
 
 @Controller('parser')
 export class ParserController {
+  private readonly logger = new Logger(ParserController.name);
+
   constructor(private readonly parserService: ParserService) {}
 
-  @Get('login')
+  @Post('login')
   async login(
-    @Query('username') username: string,
-    @Query('password') password: string,
-  ) {
-    await this.parserService.login(username, password);
-    return { message: 'Logged in successfully' };
+    @Body() body: { username: string; password: string },
+  ): Promise<string> {
+    try {
+      await this.parserService.login(body.username, body.password);
+      return '✅ Успешный логин!';
+    } catch (err) {
+      this.logger.error(`❌ Ошибка при логине: ${err.message}`);
+      return '❌ Ошибка при логине';
+    }
   }
 
-  @Get('parse')
-  async parse(
+  @Post('category')
+  async saveCategory(
+    @Body()
+    body: {
+      name: string;
+      url: string;
+      subcategories: string[];
+    },
+  ): Promise<string> {
+    try {
+      await this.parserService.saveCategory(body.name, body.url, body.subcategories);
+      return '✅ Категория сохранена/обновлена';
+    } catch (err) {
+      this.logger.error(`❌ Ошибка при сохранении категории: ${err.message}`);
+      return '❌ Ошибка при сохранении категории';
+    }
+  }
+
+  @Get('parse-topics')
+  async parseTopics(
     @Query('subcategoryUrl') url: string,
-    @Query('subcategoryName') subcategoryName: string,
-  ) {
-    await this.parserService.parseTopicsFromSubcategory(url, subcategoryName);
-    return { message: 'Parsing and saving done' };
+    @Query('subcategoryName') name: string,
+  ): Promise<string> {
+    try {
+      await this.parserService.parseTopicsFromSubcategory(url, name);
+      return `✅ Топики из подраздела "${name}" успешно распарсены и сохранены`;
+    } catch (err) {
+      this.logger.error(`❌ Ошибка при парсинге топиков: ${err.message}`);
+      return '❌ Ошибка при парсинге топиков';
+    }
   }
 }
